6ddc8b02afc84a83999a9f29c030761d67c96bc9
ronicleirps <roniclei@outlook.com>
2022-08-31 16:39:20 -0300

    Interface inicial de anuncio

--- lib/RouterGenerator.dart
+++ lib/RouterGenerator.dart
@@ -2,16 +2,24 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:olx_clone/view/anuncios.dart';
 import 'package:olx_clone/view/login.dart';
+import 'package:olx_clone/view/meusAnuncios.dart';
+import 'package:olx_clone/view/novoAnuncio.dart';
 
 class RouteGenerator {
   static Route<dynamic> generateRoute(RouteSettings settings) {
+    
     final args = settings.arguments;
 
     switch (settings.name) {
       case "/":
-        return MaterialPageRoute(builder: (_) => Anuncios());
+        return MaterialPageRoute(builder: (_) => const Anuncios());
       case "/login":
-        return MaterialPageRoute(builder: (_) => Login());
+        return MaterialPageRoute(builder: (_) => const Login());
+      case "/meus-anuncios":
+        return MaterialPageRoute(builder: (_) => const MeusAnuncios());
+        case "/novo-anuncio":
+        return MaterialPageRoute(builder: (_) => const NovoAnuncio());
+      
       default:
         _erroRota();
     }
